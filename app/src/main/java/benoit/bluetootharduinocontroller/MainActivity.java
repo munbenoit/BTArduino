93a2cab1a42557cf949a351049bfaa5380f089c8
munbenoit <mun.benoit@gmail.com>
2019-01-23 09:10:08 +0100

    Add a feature to discover devices

--- app/src/main/java/benoit/bluetootharduinocontroller/MainActivity.java
+++ app/src/main/java/benoit/bluetootharduinocontroller/MainActivity.java
@@ -2,10 +2,15 @@ package benoit.bluetootharduinocontroller;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,29 +21,44 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    public final int SCAN_ACTIVITY_REQUEST_CODE = 1;
-    private static final int REQUEST_ENABLE_BT = 1;
+
+    //identifier of the ScanActivity
+    public static final int SCAN_ACTIVITY_REQUEST_CODE = 1;
+    //identifier of the App
+    private UUID MY_UUID = UUID.fromString("f9ef9c53-fede-45fc-ade7-8900215c7342");
+
+    //UI components
     private Button onBtn;
     private Button offBtn;
     private Button scanBtn;
+
+
     private BluetoothAdapter bluetoothAdapter;
-    private ArrayList<String> devices = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
+    private BluetoothDevice selected_device;
+    private BluetoothSocket socket;
+    private ConnectionThread connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Set the layout environment
+        //Setup of the layout environment
         onBtn = (Button) findViewById(R.id.activity_main_on_button);
         offBtn = (Button) findViewById(R.id.activity_main_off_button);
         scanBtn = (Button) findViewById(R.id.activity_main_scan_button);
 
+        //Add an event
         scanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,73 +66,78 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(scanActivity, SCAN_ACTIVITY_REQUEST_CODE);
             }
         });
-        /*
-        //Register for broadcast
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        this.registerReceiver(mReceiver, filter);
-
-        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        this.registerReceiver(mReceiver, filter);
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        Log.d("Start of the App", "Start");
-        if (bluetoothAdapter == null) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Bluetooth not supported", Toast.LENGTH_LONG);
-            toast.show();
-        } else {
-            if (!bluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            } else {
-                //Store the Bounded devices
-                Log.d("Discover","Discover BOUNDED devices");
-                Set<BluetoothDevice> bltPairedDevices = bluetoothAdapter.getBondedDevices();
-                if (bltPairedDevices != null && bltPairedDevices.size() > 0) {
-                    for (BluetoothDevice bltD : bltPairedDevices) {
-                        devices.add(bltD.getAddress() + " " + bltD.getName());
-                    }
-                }
-                discoverDevices();
-            }
-        }*/
     }
 
-/*
-    private void discoverDevices(){
-        Log.d("Discover","Discover the devices");
-        if(bluetoothAdapter.isDiscovering()){
-            bluetoothAdapter.cancelDiscovery();
-        }else{
-            bluetoothAdapter.startDiscovery();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Manage the connection with ScanActivity
+        if(SCAN_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK==resultCode){
+            //Data received from ScanActivity
+            Bundle b = data.getExtras();
+            selected_device = b.getParcelable(ScanActivity.BLUETOOTH_DEVICE_SELECTED);
+            Log.d("SELECTED DEVICE",selected_device.getName());
+            //Start a new connection with a device
+            connection = new ConnectionThread(selected_device);
+            connection.run();
         }
-    }*/
-/*
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    }
 
+    /*
+    * ConnectionThread
+    * Manage the connection with the Bluetooth device
+     */
+    private class ConnectionThread extends Thread{
+        private final BluetoothSocket Blt_socket;
+        private final BluetoothDevice Blt_device;
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.d("Discover","Start of the discovering");
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if (device.getBondState()!= BluetoothDevice.BOND_BONDED){
-                    Log.d("Debug", "New device detected " + device.getName());
-                    devices.add(device.getAddress() + " " + device.getName());
+        public ConnectionThread(BluetoothDevice device){
+            BluetoothSocket tmp = null;
+            InputStream tmp_input = null;
+            Blt_device = device;
+            try{
+                Method m = Blt_device.getClass().getMethod("createInsecureRfcommSocket", new Class[] {int.class});
+                tmp = (BluetoothSocket) m.invoke(Blt_device, 1);
+            }catch (NoSuchMethodException e){
+                Log.d("DEBUG","No method");
+            }catch (IllegalAccessException e){
+                Log.d("DEBUG", "Not allowed");
+            }catch (InvocationTargetException e){
+                Log.d("DEBUG", "Not allowed");
+            }
+
+            Blt_socket = tmp;
+            //Log.d("CONNECTION SUCCESS", "Connection established with " + Blt_device.getName() + " " + Blt_device.getAddress());
+            //Toast t = Toast.makeText(getApplicationContext(), "Connection to "+ device.getName() + "established", Toast.LENGTH_SHORT);
+            //t.show();
+        }
+
+        public void run(){
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            bluetoothAdapter.cancelDiscovery();
+            try{
+                Blt_socket.connect();
+            }catch (IOException connectException){
+                try {
+                    Blt_socket.close();
+                }catch (IOException closeException){
+                    Log.e("ERROR CLOSE CONNECTION", "Couldn't close socket", closeException);
                 }
-            }else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-                Log.d("Test", "Test");
-
+                Log.e("ERROR OPEN CONNECTION", connectException.getMessage(), connectException);
+                return;
             }
         }
-    };*/
+
+        public void cancel(){
+            try {
+                Blt_socket.close();
+            }catch (IOException closeException){
+                Log.e("ERROR CLOSE CONNECTION", "Couldn't close socket", closeException);
+            }
+        }
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //unregisterReceiver(mReceiver);
     }
 }
--- app/src/main/java/benoit/bluetootharduinocontroller/ScanActivity.java
+++ app/src/main/java/benoit/bluetootharduinocontroller/ScanActivity.java
@@ -10,21 +10,37 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
 
+
+/*
+* ScanActivity
+* Manage the research of Bluetooth devices
+ */
 public class ScanActivity extends AppCompatActivity {
 
+    //identifier of the request to enable Bluetooth
     private static final int REQUEST_ENABLE_BT = 1;
+    //identifier for the chosen device
+    public static final String BLUETOOTH_DEVICE_SELECTED = "Device selection";
+
     private BluetoothAdapter bluetoothAdapter;
     private ArrayList<String> devices = new ArrayList<String>();
+    private ArrayList<BluetoothDevice> list = new ArrayList();
     private ArrayAdapter<String> adapter;
+
+    //UI components
     private ListView list_devices;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +48,29 @@ public class ScanActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_scan);
 
+        //if the user come back to the mainActivity without choosing a device
         setResult(Activity.RESULT_CANCELED);
 
+        //get the UI components
+        title = (TextView)findViewById(R.id.activity_scan_title);
         list_devices = (ListView)findViewById(R.id.activity_scan_device_list);
+        list_devices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                //add the selected device to the result
+                intent.putExtra(BLUETOOTH_DEVICE_SELECTED, list.get(position));
+                for(BluetoothDevice d : list){
+                    Log.d("DEBUG SCAN", d.getName() + " " + d.getAddress());
+                }
 
+                //send the data when the user choose a device
+                setResult(ScanActivity.RESULT_OK, intent);
+                finish();
+            }
+        });
 
+        //setup the Bluetooth research
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         this.registerReceiver(mReceiver, filter);
 
@@ -45,21 +79,23 @@ public class ScanActivity extends AppCompatActivity {
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        Log.d("Start of the App", "Start");
         if (bluetoothAdapter == null) {
             Toast toast = Toast.makeText(getApplicationContext(), "Bluetooth not supported", Toast.LENGTH_LONG);
             toast.show();
         } else {
             if (!bluetoothAdapter.isEnabled()) {
+                //Send a request box to enable Bluetooth
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             } else {
+
                 //Store the Bounded devices
                 Log.d("Discover","Discover BOUNDED devices");
                 Set<BluetoothDevice> bltPairedDevices = bluetoothAdapter.getBondedDevices();
                 if (bltPairedDevices != null && bltPairedDevices.size() > 0) {
                     for (BluetoothDevice bltD : bltPairedDevices) {
-                        devices.add(bltD.getAddress() + " " + bltD.getName());
+                        list.add(bltD);
+                        devices.add(bltD.getAddress() + " " + bltD.getName());;
                     }
                 }
                 discoverDevices();
@@ -67,6 +103,11 @@ public class ScanActivity extends AppCompatActivity {
         }
     }
 
+
+    /*
+    * discoverDevices
+    * Manage the research of the devices
+     */
     private void discoverDevices(){
         Log.d("Discover","Discover the devices");
         if(bluetoothAdapter.isDiscovering()){
@@ -78,7 +119,6 @@ public class ScanActivity extends AppCompatActivity {
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
 
-
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.d("Discover","Start of the discovering");
@@ -87,14 +127,25 @@ public class ScanActivity extends AppCompatActivity {
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if (device.getBondState()!= BluetoothDevice.BOND_BONDED){
+                if(device.getBondState() == BluetoothDevice.BOND_BONDED){
                     Log.d("Debug", "New device detected " + device.getName());
                     devices.add(device.getAddress() + " " + device.getName());
+                    list.add(device);
                 }
             }else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
                 Log.d("Test", "Test");
-                adapter = new ArrayAdapter<String>(ScanActivity.this, R.layout.activity_scan,R.id.activity_scan_device_list, devices);
+
+                if(devices.size()==0){
+                    Log.d("Debug", "Pas de devices");
+                    title.setText("No devices available");
+                }else{
+                    title.setVisibility(View.INVISIBLE);
+                }
+
+                adapter = new ArrayAdapter<String>(ScanActivity.this, android.R.layout.simple_list_item_1, devices);
                 list_devices.setAdapter(adapter);
+
+
                 for(String s : devices){
                     Log.d("Print devices", s);
                 }
